5f617d5a132e83c10d43158858a73f715048261d
Jonathan9719 <56210136+Jonathan9719@users.noreply.github.com>
2021-09-25 14:47:28 -0600

    week 2

--- Week-1/Intro/first-app.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-console.log('get ready for the file!')
-
-fs.writeFileSync('hello.txt', 'Hello from Node.js')
--- Week-1/understanding-Basics/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const routes = require('./routes')
-
-// req is the request from client, res is response from server
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
